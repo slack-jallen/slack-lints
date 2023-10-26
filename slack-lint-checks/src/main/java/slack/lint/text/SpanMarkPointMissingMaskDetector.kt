e9025284c42a797b9746c8a56f150734bf541c9b
Josh Allen <jallen@slack-corp.com>
2023-10-25 21:04:58 -1000

    modifying files

--- slack-lint-checks/src/main/java/slack/lint/text/SpanMarkPointMissingMaskDetector.kt
+++ slack-lint-checks/src/main/java/slack/lint/text/SpanMarkPointMissingMaskDetector.kt
@@ -5,20 +5,39 @@ package slack.lint.text
 import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.Category
 import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
 import com.android.tools.lint.detector.api.Issue
 import com.android.tools.lint.detector.api.JavaContext
 import com.android.tools.lint.detector.api.LintFix
+import com.android.tools.lint.detector.api.Scope
 import com.android.tools.lint.detector.api.Severity
 import com.android.tools.lint.detector.api.SourceCodeScanner
+import com.android.tools.lint.detector.api.UastCallVisitor
 import com.android.tools.lint.detector.api.UastLintUtils
+import com.android.tools.lint.detector.api.interprocedural.getTarget
+import com.intellij.openapi.command.executeCommand
+import org.jetbrains.kotlin.psi.psiUtil.parents
+import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.uast.UBinaryExpression
+import org.jetbrains.uast.UBinaryExpressionWithType
+import org.jetbrains.uast.UCallExpression
 import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.UFile
+import org.jetbrains.uast.ULambdaExpression
+import org.jetbrains.uast.UPrefixExpression
+import org.jetbrains.uast.UQualifiedReferenceExpression
 import org.jetbrains.uast.UReferenceExpression
+import org.jetbrains.uast.UThisExpression
+import org.jetbrains.uast.UUnaryExpression
 import org.jetbrains.uast.UastBinaryOperator
+import org.jetbrains.uast.getContainingUFile
 import org.jetbrains.uast.tryResolve
 import slack.lint.text.SpanMarkPointMissingMaskDetector.Companion.ISSUE
 import slack.lint.util.resolveQualifiedNameOrNull
 import slack.lint.util.sourceImplementation
+import java.util.EnumSet
+import java.util.regex.Pattern
 
 /** Checks for SpanMarkPointMissingMask. See [ISSUE]. */
 class SpanMarkPointMissingMaskDetector : Detector(), SourceCodeScanner {
@@ -43,76 +62,57 @@ class SpanMarkPointMissingMaskDetector : Detector(), SourceCodeScanner {
       )
   }
 
-  override fun getApplicableUastTypes() = listOf(UBinaryExpression::class.java)
+  override fun getApplicableUastTypes() = listOf(UFile::class.java)
 
   override fun createUastHandler(context: JavaContext): UElementHandler = ReportingHandler(context)
-}
 
-private const val SPANNED_CLASS = "android.text.Spanned"
-private val MARK_POINT_FIELDS =
-  setOf(
-    "$SPANNED_CLASS.SPAN_INCLUSIVE_INCLUSIVE",
-    "$SPANNED_CLASS.SPAN_INCLUSIVE_EXCLUSIVE",
-    "$SPANNED_CLASS.SPAN_EXCLUSIVE_INCLUSIVE",
-    "$SPANNED_CLASS.SPAN_EXCLUSIVE_EXCLUSIVE",
-  )
-private const val MASK_CLASS = "$SPANNED_CLASS.SPAN_POINT_MARK_MASK"
+
+}
 
 /** Reports violations of SpanMarkPointMissingMask. */
 private class ReportingHandler(private val context: JavaContext) : UElementHandler() {
-  override fun visitBinaryExpression(node: UBinaryExpression) {
-    if (
-      node.operator == UastBinaryOperator.EQUALS ||
-        node.operator == UastBinaryOperator.NOT_EQUALS ||
-        node.operator == UastBinaryOperator.IDENTITY_EQUALS ||
-        node.operator == UastBinaryOperator.IDENTITY_NOT_EQUALS
-    ) {
-      checkExpressions(node, node.leftOperand, node.rightOperand)
-      checkExpressions(node, node.rightOperand, node.leftOperand)
+  override fun visitFile(file: UFile) {
+      println("Checking ${file.sourcePsi.virtualFile?.presentableUrl}")
+    file.accept(object: UastCallVisitor() {
+      override fun visitCall(node: UCallExpression): Boolean {
+        if (node.methodName == "returns") {
+            val escapedText = escape(node.sourcePsi!!.text)
+            val escapedFirstArg: String
+            val escapedSecondArg: String
+            if (node.valueArguments.size == 2) {
+                escapedFirstArg = escape(node.valueArguments[0].sourcePsi!!.text)
+                escapedSecondArg = escape(node.valueArguments[1].sourcePsi!!.text)
+
+            } else {
+                escapedFirstArg = escape(node.receiver!!.sourcePsi!!.text)
+                escapedSecondArg = escape(node.valueArguments[0].sourcePsi!!.text)
+            }
+            // perl multiline replace from https://unix.stackexchange.com/a/26289
+            val args = arrayOf(
+                "perl",
+                "-0777",
+                "-i",
+                "-pe",
+                "s/$escapedText/whenever\\($escapedFirstArg\\).thenReturn\\($escapedSecondArg\\)/igs",
+                node.sourcePsi!!.containingFile.virtualFile.presentableUrl
+            )
+            println(args.joinToString(separator = " ") { "\"$it\"" })
+            Runtime.getRuntime().exec(args)
+          return true
+        }
+        return false
+      }
+    })
+      // node.uastParent?.sourcePsi ${node.uastParent?.sourcePsi?.text}
+  }
+
+    fun escape(input: String): String {
+        val specialCharacters = listOf("\\", "{", "}", "(", ")", "[", "]", ".", "+", "*", "?", "^", "$", "|", "\'", "\"")
+        var escaped = input
+        for (char in specialCharacters) {
+            escaped = escaped.replace(char, "\\$char")
+        }
+        escaped = escaped.replace("\n", "\\n")
+        return escaped
     }
-  }
-
-  private fun checkExpressions(
-    node: UBinaryExpression,
-    markPointCheck: UExpression,
-    maskCheck: UExpression
-  ) {
-    if (markPointCheck.isMarkPointFieldName() && !maskCheck.isMaskClass()) {
-      context.report(
-        ISSUE,
-        context.getLocation(node),
-        """
-          Do not check against ${markPointCheck.sourcePsi?.text} directly. \
-          Instead mask flag with Spanned.SPAN_POINT_MARK_MASK to only check MARK_POINT flags.
-        """
-          .trimIndent(),
-        LintFix.create()
-          .replace()
-          .name("Use bitwise mask")
-          .text(maskCheck.sourcePsi?.text)
-          .with("((${maskCheck.sourcePsi?.text}) and $MASK_CLASS)")
-          .build()
-      )
-    }
-  }
-}
-
-private fun UExpression.isMarkPointFieldName(): Boolean =
-  this.getQualifiedName() in MARK_POINT_FIELDS
-
-private fun UExpression.getQualifiedName(): String? {
-  return (this as? UReferenceExpression)
-    ?.referenceNameElement
-    ?.uastParent
-    ?.tryResolve()
-    ?.let(UastLintUtils::getQualifiedName)
-}
-
-private fun UExpression.isMaskClass(): Boolean {
-  return if (this is UBinaryExpression) {
-    this.leftOperand.resolveQualifiedNameOrNull() == MASK_CLASS ||
-      this.rightOperand.resolveQualifiedNameOrNull() == MASK_CLASS
-  } else {
-    false
-  }
 }
