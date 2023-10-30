b0a076d5899cc44ca928461ae3d1aa7b2050ae52
Josh Allen <jallen@slack-corp.com>
2023-10-30 11:52:12 -0400

    package name

--- slack-lint-checks/src/main/java/slack/lint/text/SpanMarkPointMissingMaskDetector.kt
+++ slack-lint-checks/src/main/java/slack/lint/text/SpanMarkPointMissingMaskDetector.kt
@@ -14,37 +14,41 @@ import com.intellij.openapi.util.TextRange
 import org.jetbrains.uast.UCallExpression
 import org.jetbrains.uast.UFile
 import org.jetbrains.uast.ULiteralExpression
+import org.jetbrains.uast.getParentOfType
 import org.jetbrains.uast.textRange
 import slack.lint.text.SpanMarkPointMissingMaskDetector.Companion.ISSUE
+import slack.lint.util.PackageName
+import slack.lint.util.isInPackageName
 import slack.lint.util.sourceImplementation
 import java.io.File
 
 /** Checks for SpanMarkPointMissingMask. See [ISSUE]. */
 class SpanMarkPointMissingMaskDetector : Detector(), SourceCodeScanner {
 
-  companion object {
-    val ISSUE =
-      Issue.create(
-        id = "SpanMarkPointMissingMask",
-        briefDescription =
-          "Check that Span flags use the bitwise mask SPAN_POINT_MARK_MASK when being compared to.",
-        explanation =
-          """
+    companion object {
+        val ISSUE =
+            Issue.create(
+                id = "SpanMarkPointMissingMask",
+                briefDescription =
+                "Check that Span flags use the bitwise mask SPAN_POINT_MARK_MASK when being compared to.",
+                explanation =
+                """
         Spans flags can have priority or other bits set. \
         Ensure that Span flags are checked using \
         `currentFlag and Spanned.SPAN_POINT_MARK_MASK == desiredFlag` \
         rather than just `currentFlag == desiredFlag`
       """,
-        category = Category.CORRECTNESS,
-        priority = 4,
-        severity = Severity.ERROR,
-        implementation = sourceImplementation<SpanMarkPointMissingMaskDetector>()
-      )
-  }
+                category = Category.CORRECTNESS,
+                priority = 4,
+                severity = Severity.ERROR,
+                implementation = sourceImplementation<SpanMarkPointMissingMaskDetector>()
+            )
+    }
 
-  override fun getApplicableUastTypes() = listOf(UFile::class.java)
+    override fun getApplicableUastTypes() = listOf(UFile::class.java)
 
-  override fun createUastHandler(context: JavaContext): UElementHandler = ReportingHandler(context)
+    override fun createUastHandler(context: JavaContext): UElementHandler =
+        ReportingHandler(context)
 
 
 }
@@ -52,41 +56,66 @@ class SpanMarkPointMissingMaskDetector : Detector(), SourceCodeScanner {
 /** Reports violations of SpanMarkPointMissingMask. */
 private class ReportingHandler(private val context: JavaContext) : UElementHandler() {
     data class Change(val range: TextRange, val newContent: String)
-  override fun visitFile(file: UFile) {
-      println("Checking ${file.sourcePsi.virtualFile?.presentableUrl}")
-      val changes = mutableListOf<Change>()
-    file.accept(object: UastCallVisitor() {
-      override fun visitCall(node: UCallExpression): Boolean {
-        if (node.methodName == "returns") {
-            val fistArg: String
-            val secondArg: String
-            if (node.valueArguments.size == 2) {
-                fistArg = node.valueArguments[0].sourcePsi!!.text
-                if (node.valueArguments[1] is ULiteralExpression) {
-                    secondArg = node.valueArguments[1].asRenderString()
-                } else {
-                    secondArg = node.valueArguments[1].sourcePsi!!.text
+
+    override fun visitFile(file: UFile) {
+        println("Checking ${file.sourcePsi.virtualFile?.presentableUrl}")
+        var changes: MutableList<Change>? = mutableListOf<Change>()
+        file.accept(object : UastCallVisitor() {
+            override fun visitCall(node: UCallExpression): Boolean {
+                if (node.methodName == "returns" && node.resolve()?.isInPackageName(
+                        PackageName(listOf("slack", "test", "mockito"))
+                    ) == true) {
+                    if (node.valueArguments.size == 2) {
+                        val fistArg = node.valueArguments[0].sourcePsi!!.text
+                        val secondArg = if (node.valueArguments[1] is ULiteralExpression) {
+                            node.valueArguments[1].asRenderString()
+                        } else {
+                            node.valueArguments[1].sourcePsi!!.text
+                        }
+                        changes?.add(
+                            Change(
+                                node.textRange!!,
+                                "whenever($fistArg).thenReturn($secondArg)"
+                            )
+                        )
+                    } else {
+                        if (node.receiver == null) {
+                            println("Missing receiver: Parent: " + node.uastParent?.sourcePsi?.text + " node " + node.sourcePsi?.text)
+                            changes = null
+                        } else {
+                            val fistArg = node.receiver!!.sourcePsi!!.text
+                            val secondArg = node.valueArguments[0].sourcePsi!!.text
+                            changes?.add(
+                                Change(
+                                    node.uastParent!!.textRange!!,
+                                    "whenever($fistArg).thenReturn($secondArg)"
+                                )
+                            )
+                        }
+                    }
+                    return true
                 }
-            } else {
-                fistArg = node.receiver!!.sourcePsi!!.text
-                secondArg = node.valueArguments[0].sourcePsi!!.text
+                return false
             }
-            changes.add(Change(node.textRange!!, "whenever($fistArg).thenReturn($secondArg)"))
-          return true
+        })
+        changes?.let { changesSnapshot ->
+            if (changesSnapshot.isNotEmpty()) {
+                println("Modifying ${file.sourcePsi.virtualFile?.presentableUrl} ${changesSnapshot.size}x")
+                changesSnapshot.sortByDescending { it.range.startOffset }
+                val fileLocation = File(file.sourcePsi.containingFile.virtualFile.presentableUrl)
+                var fileText = fileLocation.readText()
+                for (change in changesSnapshot) {
+                    fileText = fileText.replaceRange(
+                        change.range.startOffset,
+                        change.range.endOffset,
+                        change.newContent
+                    )
+                }
+                fileText = fileText.replace("import slack.test.mockito.whenever\n", "")
+                fileText = fileText.replace("import slack.test.mockito.returns\n", "import slack.test.mockito.whenever\n")
+                fileLocation.writeText(fileText)
+            }
         }
-        return false
-      }
-    })
-      if (changes.isNotEmpty()) {
-          println("Modifying ${file.sourcePsi.virtualFile?.presentableUrl} ${changes.size}x")
-          changes.sortByDescending { it.range.startOffset }
-          val fileLocation = File(file.sourcePsi.containingFile.virtualFile.presentableUrl)
-          var fileText = fileLocation.readText()
-          for (change in changes) {
-              fileText = fileText.replaceRange(change.range.startOffset, change.range.endOffset, change.newContent)
-          }
-          fileLocation.writeText(fileText)
-      }
-      // node.uastParent?.sourcePsi ${node.uastParent?.sourcePsi?.text}
-  }
+
+    }
 }
